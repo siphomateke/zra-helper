cb85bcefb7a901455239d38682886829b6d47f3c
nextgensparx <siphomateke@gmail.com>
2018-07-05 18:07:22 +0200

    Don't depend on active tab too much

--- src/background.js
+++ src/background.js
@@ -4,40 +4,48 @@ const taxTypes = ['ITX', 'VAT', 'PAYE', 'WHT'];
 
 async function getAllPendingLiabilities() {
 	try {
-		let activeTab = await getActiveTab();
-		//Account History
-		//Taxpayer Profile
-		console.log('Navigating to taxpayer profile');
-		await browser.tabs.executeScript({code: 'document.querySelector("#leftMainDiv>tbody>tr:nth-child(2)>td>div>div>div>div>div:nth-child(7)>a").click(); document.querySelector("[id=\\"4\\"]>li:nth-child(1)>div>a").click()' });
-		await tabLoaded(activeTab.id);
-		//Pending Liabilities
-		console.log('Opening pending liabilities');
-		await browser.tabs.executeScript({code: 'document.querySelector("#maincontainer>tbody>tr:nth-child(4)>td:nth-child(3)>form>fieldset:nth-child(5)>table>tbody>tr:nth-child(3)>td:nth-child(2)>a").click()'});
-		activeTab = await getActiveTab();
-		await tabLoaded(activeTab.id);
+		// TODO: Auto open ZRA tab and login
+		// Get ZRA tab
+		let matches = await browser.tabs.query({url: 'https://www.zra.org.zm/*', status: 'complete'});
+		if (matches.length > 0) {
+			// get first match
+			let tab = matches[0];
+			// Account History
+			// Taxpayer Profile
+			console.log('Navigating to taxpayer profile');
+			await browser.tabs.executeScript(tab.id, {code: 'document.querySelector("#leftMainDiv>tbody>tr:nth-child(2)>td>div>div>div>div>div:nth-child(7)>a").click(); document.querySelector("[id=\\"4\\"]>li:nth-child(1)>div>a").click()' });
+			await tabLoaded(tab.id);
+			// Pending Liabilities
+			console.log('Opening pending liabilities');
+			await browser.tabs.executeScript(tab.id, {code: 'document.querySelector("#maincontainer>tbody>tr:nth-child(4)>td:nth-child(3)>form>fieldset:nth-child(5)>table>tbody>tr:nth-child(3)>td:nth-child(2)>a").click()'});
+			tab = await getActiveTab();
+			await tabLoaded(tab.id);
 
-		for (let i=0;i<taxTypes.length;i++) {
-			const taxType = taxTypes[i];
-			console.log(`Generating ${taxType} reports`);
-			await browser.tabs.executeScript({file: './content_scripts/generate_report.js'});
-			try {
-				const response = await browser.tabs.sendMessage({
-					command: 'generateReport',
-					taxTypeId: i
-				});
-				if (response.error) {
-					throw new Error(response.error);
+			for (let i=0;i<taxTypes.length;i++) {
+				const taxType = taxTypes[i];
+				console.log(`Generating ${taxType} reports`);
+				await browser.tabs.executeScript(tab.id, {file: './content_scripts/generate_report.js'});
+				try {
+					const response = await browser.tabs.sendMessage({
+						command: 'generateReport',
+						taxTypeId: i
+					});
+					if (response.error) {
+						throw new Error(response.error);
+					}
+					// Get Totals 
+					await tabLoaded(tab.id);
+					await browser.tabs.executeScript(tab.id, {file: './content_scripts/pending_liabilities_p4.js'});
+					await browser.tabs.sendMessage(tab.id,{
+						command: "pendingLiabilitiesP4",
+						taxType,
+					});
+				} catch (error) {
+					console.log('Generating ${taxType} reports failed with error:', error);
 				}
-				//Get Totals 
-				await tabLoaded(activeTab.id);
-				await browser.tabs.executeScript({file: './content_scripts/pending_liabilities_p4.js'});
-				await browser.tabs.sendMessage(activeTab.id,{
-					command: "pendingLiabilitiesP4",
-					taxType,
-				});
-			} catch (error) {
-				console.log('Generating ${taxType} reports failed with error:', error);
 			}
+		} else {
+			throw new Error('No ZRA tabs open');
 		}
 	}
 	catch (error) {
