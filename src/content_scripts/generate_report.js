18004d245cdab77e3c832b49b24cb4e4ddca0184
nextgensparx <siphomateke@gmail.com>
2018-07-08 12:50:33 +0200

    Get ZRA error messages

--- src/content_scripts/generate_report.js
+++ src/content_scripts/generate_report.js
@@ -1,16 +1,31 @@
 console.log('generate report loaded');
 
+function getError() {
+    // Check if there is an error message on the page
+    const errorTable = document.querySelector('#maincontainer>tbody>tr:nth-child(4)>td:nth-child(3)>form>div>table>tbody>tr>td>table');
+    if (errorTable !== null) {
+        const errorTableHeader = errorTable.querySelector('tbody>tr.tdborder>td');
+        if (errorTableHeader !== null && errorTableHeader.innerText.includes('An Error has occurred')) {
+            const error = errorTable.querySelector('tbody>tr:nth-child(2)>td');
+            if (error !== null) {
+                return 'From ZRA: "'+errorTableHeader.innerText.trim()+'. '+error.innerText+'"';
+            }
+        }
+    }
+    return 'tax_type_not_found';
+}
+
 browser.runtime.onMessage.addListener((message) => {
     return new Promise((resolve, reject) => {
         if (message.command === 'generateReport') {
             // FIXME: Handle numbers being greater than 9
             const optionValue = '0'+(message.taxTypeId+1);
-            const optionExists = document.querySelector(`#prm_TaxType > option[value="${optionValue}"]`) != null;
+            const optionExists = document.querySelector(`#prm_TaxType>option[value="${optionValue}"]`) != null;
             let error = '';
             if (optionExists) {
                 document.querySelector("#prm_TaxType").value = optionValue;
             } else {
-                error = 'tax_type_not_found';
+                error = getError();
             }
             resolve({
                 taxTypeId: message.taxTypeId,
