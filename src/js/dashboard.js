1b2cfc732cd8070fcbfaed804fd2e59ee68aec3c
nextgensparx <siphomateke@gmail.com>
2018-07-16 00:41:51 +0200

    Add login and logout as tasks
    - created ClientAction class to manage logging in and out
       and a single action as tasks
    - improved the way tasks inform their parents of their progress

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -192,8 +192,13 @@ class Task {
 
         this._progress = -2;
         this._progressMax = 100;
+        /** Whether this task's state affects its parent's state */
+        this.broadcastState = true;
+        /** Whether this task will automatically update it's parent progress and status */
+        this.autoUpdateParent = true;
 
         this._complete = false;
+        // TODO: Use an enum for states
         this._state = '';
 
         /** HTML Elements */
@@ -269,6 +274,10 @@ class Task {
     set progress(progress)  {
         this._progress = progress;
         this.refreshProgress();
+
+        if (this.autoUpdateParent && this.hasParent) {
+            this.parent.refresh();
+        }
     }
     get progressMax() {
         return this._progressMax;
@@ -277,14 +286,32 @@ class Task {
         this._progressMax = max;
         this.els.progress.attr('max', this._progressMax);
     }
-    autoUpdateProgress() {
+    refresh() {
         this.progress = 0;
         this.progressMax = 0;
+        let complete = true;
+        let errors = false;
         for (const taskId of this.children) {
             const task = tasks[taskId];
             this.progress += task.progress;
             this.progressMax += task.progressMax;
+            if (task.broadcastState && task.state === 'error') {
+                errors = true;
+            }
+            if (!task.complete) {
+                complete = false;
+            }
         }
+        if (errors) {
+            this.state = 'error';
+        } else {
+            if (complete) {
+                this.state = 'success';
+            } else {
+                this.state = '';
+            }
+        }
+        this.complete = complete;
     }
     get complete() {
         return this._complete;
@@ -309,9 +336,21 @@ class Task {
         if (this._state) {
             this.els.root.addClass(this._state);
         }
+
+        if (this.autoUpdateParent && this.hasParent) {
+            this.parent.refresh();
+        }
     }
     addChild(id) {
         this.children.push(id);
+    }
+    /**
+     * Increments progress and sets status
+     * @param {string} status 
+     */
+    addStep(status) {
+        this.progress++;
+        this.status = status;
     }
 }
 
@@ -321,65 +360,147 @@ const io = new IO();
  * Creates a new tab, logs in and then closes the tab
  * @async
  * @param {Client} client 
+ * @param {Task} parentTask
  */
-async function login(client) {
+async function login(client, parentTask) {
+    const task = new Task('Login', parentTask.id);
+    task.progress = 0;
+    task.progressMax = 7;
+    task.status = 'Opening tab';
+    
     io.setCategory('login');
     io.log(`Login client "${client.name}"`);
-    const tab = await browser.tabs.create({url: 'https://www.zra.org.zm', active: false});
     try {
-        await tabLoaded(tab.id);
-        // Navigate to login page
-        await executeScript(tab.id, {code: 'document.querySelector("#leftMainDiv>tbody>tr:nth-child(2)>td>div>div>div:nth-child(2)>table>tbody>tr:nth-child(1)>td:nth-child(1)>ul>li>a").click()'}, false);
-        await tabLoaded(tab.id);
-        
-        await executeScript(tab.id, {file: 'vendor/ocrad.js'});
-        await executeScript(tab.id, {file: 'content_scripts/login.js'});
-        // Actually login
-        await browser.tabs.sendMessage(tab.id, {
-            command: 'login',
-            client,
-        });
-        await tabLoaded(tab.id);
-        await executeScript(tab.id, {file: 'content_scripts/check_login.js'});
-        const response = await browser.tabs.sendMessage(tab.id, {
-            command: 'checkLogin',
-            client,
-        });
-        if (response.error) {
-            throw new Error(response.error);
+        const tab = await browser.tabs.create({url: 'https://www.zra.org.zm', active: false});
+        task.addStep('Waiting for tab to load');
+        try {
+            await tabLoaded(tab.id);
+            task.addStep('Navigating to login page');
+            // Navigate to login page
+            await executeScript(tab.id, {code: 'document.querySelector("#leftMainDiv>tbody>tr:nth-child(2)>td>div>div>div:nth-child(2)>table>tbody>tr:nth-child(1)>td:nth-child(1)>ul>li>a").click()'}, false);
+            task.addStep('Waiting for login page to load');
+            await tabLoaded(tab.id);
+            task.addStep('Logging in');
+            
+            await executeScript(tab.id, {file: 'vendor/ocrad.js'});
+            await executeScript(tab.id, {file: 'content_scripts/login.js'});
+            // Actually login
+            await browser.tabs.sendMessage(tab.id, {
+                command: 'login',
+                client,
+            });
+            task.addStep('Waiting for login to complete');
+            await tabLoaded(tab.id);
+            task.addStep('Checking if login was successful');
+            await executeScript(tab.id, {file: 'content_scripts/check_login.js'});
+            const response = await browser.tabs.sendMessage(tab.id, {
+                command: 'checkLogin',
+                client,
+            });
+            if (response.error) {
+                throw new Error(response.error);
+            }
+            task.complete = true;
+            task.state = 'success';
+            io.log(`Done logging in "${client.name}"`);
+        } finally {
+            // Don't need to wait for the tab to close to carry out logged in actions
+            // TODO: Catch tab close errors
+            browser.tabs.remove(tab.id);
         }
-        io.log(`Done logging in "${client.name}"`);
     } catch (error) {
+        task.complete = true;
+        task.state = 'error';
+        task.status = error.message;
         throw error;
-    } finally {
-        // Don't need to wait for the tab to close to carry out logged in actions
-        browser.tabs.remove(tab.id);
     }
 }
 
 /**
  * Creates a new tab, logs out and then closes the tab
+ * @param {Client} client
+ * @param {Task} parentTask
  * @async
  */
-async function logout(client) {
+async function logout(client, parentTask) {
+    const task = new Task('Logout', parentTask.id);
+    task.progress = 0;
+    task.progressMax = 3;
+    task.status = 'Opening tab';
+
     io.setCategory('logout');
     io.log(`Logging out "${client.name}"`);
-    const tab = await browser.tabs.create({url: 'https://www.zra.org.zm/main.htm?actionCode=showHomePageLnclick', active: false});
-    await executeScript(tab.id, {code: 'document.querySelector("#headerContent>tbody>tr>td:nth-child(3)>a:nth-child(23)").click()'}, false);
-    io.log(`Done logging out "${client.name}"`);
-    // Note: The tab automatically closes after pressing logout
-    browser.tabs.remove(tab.id);
+    try {
+        const tab = await browser.tabs.create({url: 'https://www.zra.org.zm/main.htm?actionCode=showHomePageLnclick', active: false});
+        try {
+            task.addStep('Initiating logout');
+            await executeScript(tab.id, {code: 'document.querySelector("#headerContent>tbody>tr>td:nth-child(3)>a:nth-child(23)").click()'}, false);
+            task.addStep('Waiting to finish logging out');
+            task.complete = true;
+            task.state = 'success';
+            io.log(`Done logging out "${client.name}"`);
+        } finally {
+            // Note: The tab automatically closes after pressing logout
+            // TODO: Catch tab close errors
+            browser.tabs.remove(tab.id);
+        }
+    } catch (error) {
+        task.complete = true;
+        task.state = 'error';
+        task.status = error.message;
+        throw error;
+    }
 }
 
-function getAllPendingLiabilitiesAction(client) {
+class ClientAction {
+    constructor(taskName, action) {
+        this.mainTask = null;
+        this.taskName = taskName;
+        this.action = action;
+    }
+
+    async run(client) {
+        this.mainTask = new Task(client.name+': '+this.taskName);
+        this.mainTask.progress = -1;
+        try {
+            // TODO: Treat each of the following statuses as different actions with separate progresses.
+            // Keep in mind that each task may updates it's parent's progress.
+            validateClient(client);
+            this.mainTask.status = 'Logging in';
+            await login(client, this.mainTask);
+            this.mainTask.status = this.taskName;
+            await this.action(client, this.mainTask);
+            this.mainTask.status = 'Logging out';
+            await logout(client, this.mainTask);
+        } catch (error) {
+            if (debug) {
+                console.error(error);
+            }
+            let errorString = error.message;
+            if (error.message === 'client_invalid') {
+                errorString = `Row number ${i} is not a valid client`;
+            }
+            io.setCategory('client_action');
+            // TODO: Consider checking if a tab has been closed prematurely all the time.
+            // Currently, only tabLoaded checks for this.
+            io.showError(errorString);
+        } finally {
+            this.mainTask.complete = true;
+        }
+    }
+}
+
+const getAllPendingLiabilitiesAction = new ClientAction('Get all pending liabilities', 
+    /**
+     * @param {Client} client
+     * @param {Task} mainTask
+     */
+    (client, mainTask) => {
     return new Promise((resolve) => {
         let promises = [];
         /** Total number of pending liabilities including the grand total */
         const numTotals = 4;
         const totals = {};
-        const mainTask = new Task(client.name+': Get all pending liabilities');
-        mainTask.progressMax = 4 * Object.keys(taxTypes).length;
-        mainTask.progress = 0;
         io.setCategory('pending_liabilities');
         for (const taxTypeId of Object.keys(taxTypes)) {
             promises.push(new Promise(async (resolve, reject) => {
@@ -387,6 +508,8 @@ function getAllPendingLiabilitiesAction(client) {
                 const task = new Task(`Get ${taxType} totals`, mainTask.id);
                 task.progress = 0;
                 task.progressMax = 4;
+                // Failing to retrieve a tax type is not really an error so don't tell our parent task
+                task.broadcastState = false;
                 task.status = 'Opening tab';
 
                 io.log(`Generating ${taxType} report`);
@@ -399,16 +522,12 @@ function getAllPendingLiabilitiesAction(client) {
                     });
                     await tabLoaded(tab.id);
 
-                    task.status = 'Selecting tax type';
-                    task.progress++;
-                    mainTask.autoUpdateProgress();
+                    task.addStep('Selecting tax type');
 
                     await executeScript(tab.id, {file: 'content_scripts/generate_report.js'});
 
                     try {
-                        task.status = 'Generating report';
-                        task.progress++;
-                        mainTask.autoUpdateProgress();
+                        task.addStep('Generating report');
 
                         const response = await browser.tabs.sendMessage(tab.id, {
                             command: 'generateReport',
@@ -420,9 +539,7 @@ function getAllPendingLiabilitiesAction(client) {
                         // Get Totals
                         await tabLoaded(tab.id);
 
-                        task.status = 'Getting totals';
-                        task.progress++;
-                        mainTask.autoUpdateProgress();
+                        task.addStep('Getting totals');
 
                         await executeScript(tab.id, {file: 'content_scripts/get_totals.js'});
                         const totalsResponse = await browser.tabs.sendMessage(tab.id,{
@@ -434,7 +551,6 @@ function getAllPendingLiabilitiesAction(client) {
                         totals[taxType] = totalsResponse.totals;
                         task.complete = true;
                         task.state = 'success';
-                        mainTask.autoUpdateProgress();
                         resolve();
                     } finally {
                         io.log(`Finished generating ${taxType} report`);
@@ -446,12 +562,13 @@ function getAllPendingLiabilitiesAction(client) {
                     resolve();
                     let errorString = error.message;
                     if (error.message === 'tax_type_not_found') {
+                        // TODO: Don't show the tax type if it's under a task which
+                        // already has the tax type in it's title
                         errorString = taxType+' tax type not found';
                     }
                     task.complete = true;
                     task.state = 'error';
                     task.status = errorString;
-                    mainTask.autoUpdateProgress();
                     io.showError(errorString);
                 } finally {
                     if (tab) {
@@ -465,7 +582,6 @@ function getAllPendingLiabilitiesAction(client) {
             }));
         }
         Promise.all(promises).then(() => {
-            mainTask.state = 'success';
             const rows = [];
             let i = 0;
             for (const taxType of Object.values(taxTypes)) {
@@ -490,28 +606,17 @@ function getAllPendingLiabilitiesAction(client) {
             resolve();
         });
     });
-}
+});
 
+/**
+ * 
+ * @param {ClientAction} action 
+ */
 async function allClientsAction(action) {
     if (clientList.length > 0) {
         for (let i=0;i<clientList.length;i++) {
             const client = clientList[i];
-            try {
-                validateClient(client);
-                await login(client);
-                await action(client);
-                await logout(client);
-            } catch (error) {
-                if (debug) {
-                    console.error(error);
-                }
-                let errorString = error.message;
-                if (error.message === 'client_invalid') {
-                    errorString = `Row number ${i} is not a valid client`;
-                }
-                io.setCategory('client_action');
-                io.showError(errorString);
-            }
+            await action.run(client);
         }
     } else {
         io.setCategory('client_action');
