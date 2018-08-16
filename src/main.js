e8c315fb424730d708311ba4911e8fe84e7796f6
nextgensparx <siphomateke@gmail.com>
2018-08-17 00:04:26 +0200

    Add devtools

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+// NOTE: devtools must be imported before vue
+import devtools from '@vue/devtools'; // eslint-disable-line import/no-extraneous-dependencies
 import Vue from 'vue';
 import Buefy from 'buefy';
 import App from './Dashboard.vue';
 import router from './router';
 import store from './store';
+
+if (process.env.NODE_ENV === 'development') {
+  devtools.connect();
+}
 
 Vue.config.productionTip = false;
 
