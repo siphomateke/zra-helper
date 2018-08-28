47981ba5ca8b719ac2c9921678fc7ec63f9bfa0e
nextgensparx <siphomateke@gmail.com>
2018-08-28 15:28:14 +0200

    Revert "Fix incorrect usage of global module"
    The original usage was actually correct. 
    
    This reverts commit 12890a8b2c00986b2dee909de47eafdc7fe84d44.

--- src/store/modules/log.js
+++ src/store/modules/log.js
@@ -45,7 +45,7 @@ export default {
     addLine({ commit, state, rootState }, payload) {
       commit('addLine', payload);
 
-      if (rootState.debug) {
+      if (rootState.global.debug) {
         const text = `${payload.category || state.currentCategory}: ${payload.content}`;
         switch (payload.type) {
           case 'error':
@@ -80,7 +80,7 @@ export default {
         type: warning ? 'warning' : 'error',
       });
 
-      if (rootState.debug && error instanceof Error) {
+      if (rootState.global.debug && error instanceof Error) {
         if (error instanceof ExtendedError) {
           console.groupCollapsed(`${error.type} Details`);
           console.log(error.stack);
