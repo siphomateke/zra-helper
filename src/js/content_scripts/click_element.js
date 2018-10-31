5254fb17aaad8580cdfe34830c69e0178a070895
nextgensparx <siphomateke@gmail.com>
2018-10-31 12:08:51 +0200

    Make sure the missing selector error is sent back

--- src/js/content_scripts/click_element.js
+++ src/js/content_scripts/click_element.js
@@ -1,19 +1,19 @@
 import { errorToJson } from '../errors';
 import { getElement } from './helpers/elements';
 
-browser.runtime.onMessage.addListener(message => new Promise((resolve, reject) => {
+browser.runtime.onMessage.addListener(message => new Promise((resolve) => {
   if (message.command === 'click') {
-    if (message.selector) {
-      try {
+    try {
+      if (message.selector) {
         // This must be before resolve so we can send back any caught errors.
         const element = getElement(message.selector, message.name);
         resolve({});
         element.click();
-      } catch (error) {
-        resolve({ error: errorToJson(error) });
+      } else {
+        throw new Error('A selector for an element to be clicked must be provided.');
       }
-    } else {
-      reject(new Error('A selector for an element to be clicked must be provided.'));
+    } catch (error) {
+      resolve({ error: errorToJson(error) });
     }
   }
 }));
