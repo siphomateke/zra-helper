24e620dbae5b82d98b8a6157010e50a14924ad9d
nextgensparx <siphomateke@gmail.com>
2018-11-20 22:44:07 +0200

    Add vuex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue';
 import Buefy from 'buefy';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 if (process.env.NODE_ENV === 'development') {
   devtools.connect();
@@ -17,5 +18,6 @@ Vue.use(Buefy, {
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
