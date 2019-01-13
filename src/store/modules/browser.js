d6267f06461f1a41e8d36ba3c9bfd7c2efe8a664
nextgensparx <siphomateke@gmail.com>
2019-01-13 20:40:36 +0200

    Lint

--- src/store/modules/browser.js
+++ src/store/modules/browser.js
@@ -174,10 +174,17 @@ export default {
         // If the extension does not have permission to execute a script on this tab,
         // then this tab is probably the browser error page which usually only
         // shows up when the user is offline.
-        if (error.message && (
-          error.message.includes('Cannot access contents of url "chrome-error://chromewebdata/"') ||
-                error.message.includes('Missing host permission for the tab'))) {
-          throw new ExecuteScriptError(`Cannot access tab with ID ${tabId}. Please check your internet connection and try again.`, 'NoAccess', { tabId });
+        if (error.message) {
+          if (
+            error.message.includes('Cannot access contents of url "chrome-error://chromewebdata/"')
+            || error.message.includes('Missing host permission for the tab')
+          ) {
+            throw new ExecuteScriptError(
+              `Cannot access tab with ID ${tabId}. Please check your internet connection and try again.`,
+              'NoAccess',
+              { tabId },
+            );
+          }
         }
         throw new ExecuteScriptError(`Failed to execute script: "${errorString}"`, null, { tabId });
       }
@@ -199,7 +206,8 @@ export default {
      * @param {ActionContext} context
      * @param {Object} payload
      * @param {number} payload.desiredTabId
-     * @param {number} [payload.timeout] The amount of time to wait for a tab to load (in milliseconds). Default value is the one set in config.
+     * @param {number} [payload.timeout]
+     * The amount of time to wait for a tab to load (in milliseconds). Default value is the one set in config.
      * @returns {Promise}
      * @throws {TabError} Throws an error if the tab is closed before it loads
      */
@@ -207,6 +215,7 @@ export default {
       if (timeout === null) timeout = rootState.config.tabLoadTimeout;
 
       return new Promise((resolve, reject) => {
+        let removeListeners;
         function updatedListener(tabId, changeInfo) {
           if (tabId === desiredTabId && changeInfo.status === 'complete') {
             removeListeners();
@@ -216,13 +225,17 @@ export default {
         function removedListener(tabId) {
           if (tabId === desiredTabId) {
             removeListeners();
-            reject(new TabError(`Tab with ID ${tabId} was closed before it could finish loading.`, 'Closed', { tabId }));
+            reject(new TabError(
+              `Tab with ID ${tabId} was closed before it could finish loading.`,
+              'Closed',
+              { tabId },
+            ));
           }
         }
-        function removeListeners() {
+        removeListeners = function removeListeners() {
           browser.tabs.onUpdated.removeListener(updatedListener);
           browser.tabs.onRemoved.removeListener(removedListener);
-        }
+        };
 
         browser.tabs.onUpdated.addListener(updatedListener);
         browser.tabs.onRemoved.addListener(removedListener);
