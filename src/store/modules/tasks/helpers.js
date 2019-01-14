a197982b3cdcab94012ce58db81516e3199c1462
nextgensparx <siphomateke@gmail.com>
2019-01-14 11:16:21 +0200

    Improve vuex task wrapper

--- src/store/modules/tasks/helpers.js
+++ src/store/modules/tasks/helpers.js
@@ -8,7 +8,7 @@ import { getListItemStore } from '@/store/helpers/list_store';
  * @param {number} id
  * @returns {import('@/store/helpers/list_store').ListItemStore}
  */
-export function taskFromId(store, id) {
+function taskFromId(store, id) {
   return getListItemStore(store, {
     id,
     namespace: 'tasks',
@@ -16,12 +16,97 @@ export function taskFromId(store, id) {
   });
 }
 
+/** @typedef {import('@/store/modules/tasks').TaskState} TaskState */
+
 /**
- * Creates a new task.
- * @param {VuexStore} store
- * @param {import('@/store/modules/tasks').TaskState} data
+ * @typedef {function} Task.setError
+ * @param {Error} error
  */
-export async function createTask(store, data) {
-  const id = await store.dispatch('tasks/create', data);
-  return taskFromId(store, id);
+
+/**
+ * @typedef {function} Task.addStep
+ * @param {string} status
+ * @param {number} increment
+ */
+
+/**
+ * @typedef TaskObjectType
+ * @property {boolean} hasParent
+ * @property {TaskObject} parent
+ * @property {boolean} hasChildren
+ * @property {number[]} children
+ * @property {Object.<TaskState, number>} childStateCounts Total number of child states per state type
+ * @property {string} childStateString
+ * @property {string} getStatusFromError
+ * @property {boolean} complete
+ * @property {number} progress
+ * @property {number} progressMax
+ *
+ * @property {Task.setError} setError
+ * @property {Task.addStep} addStep
+ */
+
+/**
+ * @typedef {import('@/store/modules/tasks').TaskVuexState} TaskVuexState
+ */
+
+/**
+ * @typedef {TaskVuexState & TaskObjectType} TaskObject
+ * Wrapper around the task Vuex module to make it compatible with legacy code.
+ */
+
+class Task {
+  /**
+   * Creates a new task.
+   * @param {VuexStore} store
+   * @param {TaskVuexState} data
+   */
+  constructor(store, data) {
+    store.dispatch('tasks/create', data).then((id) => {
+      this.listStoreTask = taskFromId(store, id);
+    });
+  }
+
+  setError(error) {
+    this.listStoreTask.setError({ error });
+  }
+
+  addStep(status, increment = null) {
+    const options = { status };
+    if (increment !== null) {
+      options.increment = increment;
+    }
+    this.listStoreTask.addStep(options);
+  }
+}
+
+/**
+ * TODO: Document this
+ * @param {VuexStore} store
+ * @param {TaskVuexState} data
+ * @return {TaskObject}
+ */
+export default function createTask(store, data) {
+  const task = new Task(store, data);
+  return new Proxy(task, {
+    /**
+     * @param {Task} obj
+     * @param {string} prop
+     */
+    get(obj, prop) {
+      if (typeof prop === 'string') {
+        return obj.listStoreTask[prop];
+      }
+      return Reflect.get(...arguments); // eslint-disable-line prefer-rest-params
+    },
+    /**
+     * @param {Task} obj
+     * @param {string} prop
+     * @param {*} value
+     */
+    set(obj, prop, value) {
+      obj.listStoreTask[prop] = value;
+      return true;
+    },
+  });
 }
