efb981c30043a79bc25b1e4cc0649aeb43e0d6c0
siphomateke <siphomateke@gmail.com>
2019-02-24 12:47:11 +0200

    Move login and logout functions to store

--- src/store/modules/client_actions/index.js
+++ src/store/modules/client_actions/index.js
@@ -3,9 +3,9 @@ import store from '@/store';
 import log from '@/transitional/log';
 import createTask from '@/transitional/tasks';
 import { taskStates } from '@/store/modules/tasks';
-import { logout, robustLogin } from '@/backend/client_actions/base';
 import { writeJson } from '@/backend/file_utils';
 import { InvalidClientError } from '@/backend/errors';
+import { clickElement, createTab, executeScript, sendMessage, tabLoaded, closeTab } from '@/backend/utils';
 
 /**
  * @typedef {import('vuex').ActionContext} ActionContext
@@ -125,6 +125,172 @@ const module = {
 
       commit('add', payloadCopy);
     },
+
+    /**
+     * Creates a new tab, logs in and then closes the tab
+     *
+     * @param {ActionContext} context
+     * @param {Object} payload
+     * @param {Client} payload.client
+     * @param {number} payload.parentTaskId
+     * @returns {Promise}
+     * @throws {import('@/backend/errors').ExtendedError}
+     */
+    async login(context, { client, parentTaskId }) {
+      const task = await createTask(store, {
+        title: 'Login',
+        parent: parentTaskId,
+        progressMax: 7,
+        status: 'Opening tab',
+      });
+
+      log.setCategory('login');
+      log.log(`Logging in client "${client.name}"`);
+      try {
+        const tab = await createTab('https://www.zra.org.zm');
+        task.addStep('Waiting for tab to load');
+        try {
+          await tabLoaded(tab.id);
+          task.addStep('Navigating to login page');
+          // Navigate to login page
+          await clickElement(
+            tab.id,
+            // eslint-disable-next-line max-len
+            '#leftMainDiv>tbody>tr:nth-child(2)>td>div>div>div:nth-child(2)>table>tbody>tr:nth-child(1)>td:nth-child(1)>ul>li>a',
+            'go to login button',
+          );
+          task.addStep('Waiting for login page to load');
+          await tabLoaded(tab.id);
+          task.addStep('Logging in');
+          // OCRAD should be imported in login.js but work with webpack
+          await executeScript(tab.id, { file: 'ocrad.js' }, true);
+          await executeScript(tab.id, { file: 'login.js' });
+          // Actually login
+          await sendMessage(tab.id, {
+            command: 'login',
+            client,
+            maxCaptchaRefreshes: 10,
+          });
+          task.addStep('Waiting for login to complete');
+          await tabLoaded(tab.id);
+          task.addStep('Checking if login was successful');
+          await executeScript(tab.id, { file: 'check_login.js' });
+          await sendMessage(tab.id, {
+            command: 'checkLogin',
+            client,
+          });
+          /* const tokenKey = await sendMessage(tab.id, { command: 'getSessionData' });
+          console.log('Token key: ', tokenKey); */
+          task.state = taskStates.SUCCESS;
+          log.log(`Done logging in "${client.name}"`);
+        } finally {
+          // Don't need to wait for the tab to close to carry out logged in actions
+          // TODO: Catch tab close errors
+          closeTab(tab.id);
+        }
+      } catch (error) {
+        task.setError(error);
+        throw error;
+      } finally {
+        task.markAsComplete();
+      }
+    },
+
+    /**
+     * Creates a new tab, logs out and then closes the tab
+     *
+     * @param {ActionContext} context
+     * @param {Object} payload
+     * @param {number} payload.parentTaskId
+     * @returns {Promise}
+     */
+    async logout(context, { parentTaskId }) {
+      const task = await createTask(store, {
+        title: 'Logout',
+        parent: parentTaskId,
+        unknownMaxProgress: false,
+        progressMax: 3,
+        status: 'Opening tab',
+      });
+
+      log.setCategory('logout');
+      log.log('Logging out');
+      try {
+        const tab = await createTab('https://www.zra.org.zm/main.htm?actionCode=showHomePageLnclick');
+        try {
+          task.addStep('Initiating logout');
+          // Click logout button
+          await clickElement(tab.id, '#headerContent>tbody>tr>td:nth-child(3)>a:nth-child(23)', 'logout button');
+          task.addStep('Waiting to finish logging out');
+          task.state = taskStates.SUCCESS;
+          log.log('Done logging out');
+        } finally {
+          // Note: The tab automatically closes after pressing logout
+          // TODO: Catch tab close errors
+          closeTab(tab.id);
+        }
+      } catch (error) {
+        task.setError(error);
+        throw error;
+      } finally {
+        task.markAsComplete();
+      }
+    },
+
+    /**
+     * Logs in a client and retries if already logged in as another client
+     * @param {ActionContext} context
+     * @param {Object} payload
+     * @param {Client} payload.client
+     * @param {number} payload.parentTaskId
+     * @param {number} payload.maxAttempts The maximum number of times an attempt should be made to login to a client.
+     */
+    async robustLogin({ dispatch }, { client, parentTaskId, maxAttempts }) {
+      const task = await createTask(store, {
+        title: 'Robust login',
+        parent: parentTaskId,
+        unknownMaxProgress: false,
+        // After every login attempt except the last one, we logout.
+        // So if the maximum number of login attempts is 3, we login 3 times but only logout 2 times.
+        // Thus the total number of tasks would be 3 + (3-1) = 5
+        progressMax: maxAttempts + (maxAttempts - 1),
+      });
+      let attempts = 0;
+      let run = true;
+      try {
+        /* eslint-disable no-await-in-loop */
+        while (run) {
+          try {
+            if (attempts > 0) {
+              task.status = 'Logging in again';
+            } else {
+              task.status = 'Logging in';
+            }
+            await dispatch('login', { client, parentTaskId: task.id });
+            run = false;
+          } catch (error) {
+            if (error.type === 'LoginError' && error.code === 'WrongClient' && attempts + 1 < maxAttempts) {
+              log.setCategory('login');
+              log.showError(error, true);
+              task.status = 'Logging out';
+              await dispatch('logout', { parentTaskId: task.id });
+              run = true;
+            } else {
+              throw error;
+            }
+          }
+          attempts++;
+        }
+        /* eslint-enable no-await-in-loop */
+        task.state = taskStates.SUCCESS;
+      } catch (error) {
+        task.setError(error);
+        throw error;
+      } finally {
+        task.markAsComplete();
+      }
+    },
+
     /**
      * Runs an action on a single client.
      * @param {ActionContext} context
@@ -213,7 +379,11 @@ const module = {
       try {
         if (client.valid) {
           mainTask.status = 'Logging in';
-          await robustLogin(client, mainTask.id, rootState.config.maxLoginAttempts);
+          await dispatch('robustLogin', {
+            client,
+            parentTaskId: mainTask.id,
+            maxAttempts: rootState.config.maxLoginAttempts,
+          });
 
           // Run actions in parallel
           if (!isSingleAction) {
@@ -233,7 +403,7 @@ const module = {
           await Promise.all(promises);
 
           mainTask.status = 'Logging out';
-          await logout(mainTask.id);
+          await dispatch('logout', { parentTaskId: mainTask.id });
 
           if (mainTask.state !== taskStates.ERROR && mainTask.state !== taskStates.WARNING) {
             if (mainTask.childStateCounts[taskStates.WARNING] > 0) {
