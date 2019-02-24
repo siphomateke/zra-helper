776e08a5c78c0b118cf3365d0d98558e6cea758c
siphomateke <siphomateke@gmail.com>
2019-02-24 14:46:58 +0200

    Remove accidentally committed ClientActionObject property

--- src/backend/constants.js
+++ src/backend/constants.js
@@ -62,9 +62,6 @@ export const clientPropValidationErrorMessages = {
  * @property {string} name The human-readable name of this client action.
  * @property {ClientActionFunction} [func]
  * @property {boolean} hasOutput Whether this client action returns an output.
- * @property {boolean} usesProfilePage Whether this action needs to open a page on the user's profile.
- * If this is enabled, the page that is opened after logging in will not be closed until the user is
- * about to be logged out.
  * @property {ClientActionOutputFormat} defaultOutputFormat
  * @property {ClientActionOutputFormatter} outputFormatter
  * Function that formats the output into different formats such as CSV and JSON.
