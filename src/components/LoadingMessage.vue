f5e8e19a91decff88ade23139f839a175bae35a9
siphomateke <siphomateke@gmail.com>
2019-03-10 13:13:23 +0200

    Add loading message component

---
+++ src/components/LoadingMessage.vue
@@ -0,0 +1,29 @@
+<template>
+  <CenterMessage
+    :message="message"
+    :icon="icon"
+  >
+    <div class="spinner is-centered"/>
+  </CenterMessage>
+</template>
+
+<script>
+import CenterMessage from '@/components/CenterMessage.vue';
+
+export default {
+  name: 'LoadingMessage',
+  components: {
+    CenterMessage,
+  },
+  props: {
+    message: {
+      type: String,
+      default: '',
+    },
+    icon: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
