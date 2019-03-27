ac4cf0d0f7d29961c10627a88e9847d900eb9677
siphomateke <siphomateke@gmail.com>
2019-03-27 16:58:40 +0200

    Install Vue CLI Typescript plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:vue/recommended',
     '@vue/airbnb',
+    '@vue/typescript',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -29,6 +30,6 @@ module.exports = {
     },
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
   },
 };
