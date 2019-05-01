5b547d529faacfb09b2c7b3614c641d2e3b7f5f0
siphomateke <siphomateke@gmail.com>
2019-05-01 19:28:04 +0200

    Setup Jest for testing tax payer ledger client action logic

--- jest.config.js
+++ jest.config.js
@@ -20,11 +20,14 @@ module.exports = {
     'jest-serializer-vue',
   ],
   testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)|**/*.(spec|test).(js|jsx|ts|tsx)',
   ],
   testURL: 'http://localhost/',
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
+  ],
+  setupFiles: [
+    '<rootDir>/tests/setupTests.js',
   ],
 };
