8c3d47bf0b41426d728f14be0bec4b7c6473abe1
siphomateke <siphomateke@gmail.com>
2019-05-21 10:05:29 +0200

    Split totalsColumns into pending liability types and columns

--- src/backend/client_actions/pending_liabilities.js
+++ src/backend/client_actions/pending_liabilities.js
@@ -10,17 +10,25 @@ import {
 import { getPendingLiabilityPage } from '../reports';
 import { errorToString } from '../errors';
 
-/** Columns to get from the pending liabilities table */
-const totalsColumns = [
+/** @typedef {'principal'|'interest'|'penalty'} PendingLiabilityType */
+
+/** @type {PendingLiabilityType[]} */
+export const pendingLiabilityTypes = [
   'principal',
   'interest',
   'penalty',
+];
+
+/** Columns to get from the pending liabilities table */
+export const pendingLiabilityColumns = [
+  ...pendingLiabilityTypes,
   'total',
 ];
 
 /**
  * @typedef {Object.<string, string>} Totals
- * Totals with two decimal places. The possible totals are all the items in `totalsColumns`.
+ * Totals with two decimal places. The possible totals are all the items in
+ * `pendingLiabilityColumns`.
  */
 
 /**
@@ -90,7 +98,7 @@ async function getPendingLiabilities(client, taxTypeId, parentTaskId) {
         // Make sure we are getting totals from the grand total row.
         if (totalsRow.srNo.toLowerCase() === 'grand total') {
           totals = {};
-          for (const column of totalsColumns) {
+          for (const column of pendingLiabilityColumns) {
             const cell = totalsRow[column];
             totals[column] = cell.replace(/\n\n/g, '');
           }
@@ -98,7 +106,7 @@ async function getPendingLiabilities(client, taxTypeId, parentTaskId) {
           totals = null;
         }
       } else {
-        totals = generateTotals(totalsColumns, '0');
+        totals = generateTotals(pendingLiabilityColumns, '0');
       }
 
       return totals;
@@ -115,9 +123,9 @@ function outputFormatter({
 }) {
   if (format === exportFormatCodes.CSV) {
     const rows = [];
-    const columnOrder = totalsColumns;
+    const columnOrder = pendingLiabilityColumns;
     // Columns are: client identifier, ...totals, error
-    const numberOfColumns = 2 + totalsColumns.length + 1;
+    const numberOfColumns = 2 + pendingLiabilityColumns.length + 1;
 
     const allClientsById = new Map();
     for (const client of allClients) {
@@ -264,8 +272,8 @@ export function csvOutputParser(csvString) {
     }
     const taxType = row[1];
     const taxTypeTotals = {};
-    for (let i = 0; i < totalsColumns.length; i++) {
-      taxTypeTotals[totalsColumns[i]] = row[i + 2];
+    for (let i = 0; i < pendingLiabilityColumns.length; i++) {
+      taxTypeTotals[pendingLiabilityColumns[i]] = row[i + 2];
     }
     totals[taxType] = taxTypeTotals;
   }
