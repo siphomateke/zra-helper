b15ce060c8f1c8bc8d54962a299795ad3fd94e56
siphomateke <siphomateke@gmail.com>
2019-06-03 10:39:37 +0200

    Allow choosing which tax accounts to run ledger action on

--- src/components/ClientActionInputs/TaxPayerLedgerInput.vue
+++ src/components/ClientActionInputs/TaxPayerLedgerInput.vue
@@ -5,23 +5,31 @@
       :disabled="disabled"
       @input="dateRangeInput"
     />
+    <TaxTypeSelect
+      v-model="input.taxTypeIds"
+      :disabled="disabled"
+      :multiple="true"
+    />
   </div>
 </template>
 
 <script>
 import DateRangeInput from '@/components/fields/DateRangeInput.vue';
+import TaxTypeSelect from '@/components/fields/TaxTypeSelect.vue';
 import ClientActionInputMixin from './mixin';
 
 export default {
   name: 'ClientActionTaxPayerLedgerInput',
   components: {
     DateRangeInput,
+    TaxTypeSelect,
   },
   mixins: [ClientActionInputMixin],
   props: {
     value: {
       type: Object,
       default: () => ({
+        taxTypeIds: [],
         fromDate: null,
         toDate: null,
       }),
