a6dad2b4fe438632ee1fa92f40e81dcda6090391
siphomateke <siphomateke@gmail.com>
2019-06-06 00:37:58 +0200

    Allow viewing output while running and remove output preview
    Closes #141

--- src/components/ClientActionOutput.vue
+++ src/components/ClientActionOutput.vue
@@ -3,75 +3,14 @@
     <div class="field">
       <label class="label">{{ action.name }} output</label>
       <div class="bordered-section">
-        <div v-if="!loading">
-          <ExportButtons
-            v-if="output"
-            :generators="generators"
-            :default-format="defaultFormat"
-            :filename="`${action.id}Output`"
-          />
-          <b-field label="Output preview format">
-            <div
-              v-if="action.outputFormats.length < 8"
-              class="control"
-            >
-              <b-radio
-                v-for="format of action.outputFormats"
-                :key="format"
-                :native-value="format"
-                v-model="selectedOutputFormat"
-              >{{ getFormatName(format) }}</b-radio>
-            </div>
-            <b-select
-              v-else
-              v-model="selectedOutputFormat"
-            >
-              <option
-                v-for="format of action.outputFormats"
-                :key="format"
-                :value="format"
-              >{{ getFormatName(format) }}</option>
-            </b-select>
-          </b-field>
-          <div
-            v-if="output"
-            class="control client-action-output-control"
-          >
-            <template v-if="outputGenerated">
-              <ExportViewer
-                :raw="displayRawOutput"
-                :format="selectedOutputFormat"
-                :output="output"
-              />
-              <b-field>
-                <b-checkbox
-                  v-if="selectedOutputFormat !== exportFormatCodes.TXT"
-                  v-model="displayRawOutput"
-                >Show raw output</b-checkbox>
-              </b-field>
-            </template>
-            <div
-              v-else-if="!outputGenerationErrorMessage"
-              class="bordered-section"
-            >
-              <LoadingMessage message="Generating output preview"/>
-            </div>
-            <b-message
-              v-if="outputGenerationErrorMessage"
-              type="is-danger"
-              title="Error generating output"
-            >{{ outputGenerationErrorMessage }}</b-message>
-          </div>
-          <div
-            v-else
-            class="bordered-section"
-          >
-            <EmptyMessage message="Output is empty"/>
-          </div>
-        </div>
         <LoadingMessage
-          v-else
+          v-if="loading"
           message="Getting output"
+        />
+        <ExportButtons
+          :generators="generators"
+          :default-format="defaultFormat"
+          :filename="`${action.id}Output`"
         />
       </div>
     </div>
@@ -83,9 +22,6 @@ import ExportButtons from '@/components/ExportData/ExportButtons.vue';
 import EmptyMessage from '@/components/EmptyMessage.vue';
 import LoadingMessage from '@/components/LoadingMessage.vue';
 import { mapGetters } from 'vuex';
-import { exportFormats, exportFormatCodes } from '../backend/constants';
-import ExportViewer from './ExportData/ExportViewer.vue';
-import { errorToString } from '../backend/errors';
 
 export default {
   name: 'ClientActionOutput',
@@ -93,7 +29,6 @@ export default {
     ExportButtons,
     EmptyMessage,
     LoadingMessage,
-    ExportViewer,
   },
   props: {
     runId: {
@@ -105,21 +40,9 @@ export default {
       default: '',
     },
   },
-  data() {
-    return {
-      output: null,
-      selectedOutputFormat: null,
-      clientOutputs: null,
-      outputGenerated: false,
-      exportFormatCodes,
-      displayRawOutput: false,
-      outputGenerationError: null,
-    };
-  },
   computed: {
     ...mapGetters('clientActions', [
       'getActionById',
-      'getInstanceById',
       'getOutputsOfAction',
       'actionHasOutput',
     ]),
@@ -158,44 +81,11 @@ export default {
       }
       return generators;
     },
-    outputGenerationErrorMessage() {
-      if (this.outputGenerationError) {
-        return errorToString(this.outputGenerationError);
-      }
-      return null;
-    },
-  },
-  watch: {
-    selectedOutputFormat() {
-      this.outputGenerated = false;
-      this.getOutput();
-    },
-    defaultFormat: {
-      immediate: true,
-      handler(format) {
-        this.selectedOutputFormat = format;
-      },
-    },
     clientOutputs() {
-      this.getOutput();
+      return this.getOutputsOfAction(this.runId, this.actionId);
     },
-    loading(loading) {
-      if (!loading) {
-        this.updateOutput();
-      }
-    },
-  },
-  created() {
-    this.getOutput();
   },
   methods: {
-    /**
-     * Gets the outputs of all this action's clients.
-     * @returns {import('@/store/modules/client_actions').ClientActionOutputs}
-     */
-    getClientOutputs() {
-      this.clientOutputs = this.getOutputsOfAction(this.runId, this.actionId);
-    },
     /**
      * @param {import('@/backend/constants').ExportFormatCode} format
      */
@@ -207,23 +97,6 @@ export default {
         format,
         anonymizeClients,
       });
-    },
-    async getOutput() {
-      if (this.clientOutputs) {
-        try {
-          this.output = await this.formatOutput(this.selectedOutputFormat);
-          this.outputGenerated = true;
-          this.outputGenerationError = false;
-        } catch (error) {
-          this.outputGenerationError = error;
-        }
-      }
-    },
-    updateOutput() {
-      this.getClientOutputs();
-    },
-    getFormatName(format) {
-      return exportFormats[format].name;
     },
   },
 };
