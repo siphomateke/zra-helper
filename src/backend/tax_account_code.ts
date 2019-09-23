24ecbef61a4715c1f4c03d6c0ef69adab653bb4c
siphomateke <siphomateke@gmail.com>
2019-09-24 00:49:36 +0200

    Separate non-task logic from getAccountCodeTask

--- src/backend/tax_account_code.ts
+++ src/backend/tax_account_code.ts
@@ -25,17 +25,61 @@ interface AccountData {
   value: TaxAccountName;
 }
 
-interface GetAccountCodeTaskFnOptions {
+interface GetAccountCodeFnOptions {
   /** The name of the account whose code we would like to know. */
   accountName: TaxAccountName;
   /** The numerical tax type ID of the account whose code we would like to know. */
   taxTypeId: TaxTypeNumericalCode;
+}
+
+interface GetAccountCodeTaskFnOptions extends GetAccountCodeFnOptions {
   parentTaskId: TaskId;
 }
 
 /**
  * Gets the code of an account whose name and tax type is known. The reason we would want the code
  * of an account is to generate reports for that account.
+ * @returns The account code.
+ */
+export async function getTaxAccountCode({
+  accountName,
+  taxTypeId,
+}: GetAccountCodeFnOptions): Promise<TaxAccountCode> {
+  // Get the codes and names of all tax accounts that have the specified tax type.
+  const response = await xmlRequest({
+    url: `${ZraDomain}/frontController.do?actionCode=RPRTPAJAXCHILDCOMBO`,
+    method: 'post',
+    data: {
+      prm_ajaxComboTarget: 'accountName',
+      reportCode: ReportCode.PENDING_LIABILITY,
+      prm_TaxType: taxTypeId,
+    },
+  });
+
+  let accountData: AccountData | AccountData[] | undefined = response['request-params'].param;
+  let accountNameNotFound: boolean = false;
+  if (Array.isArray(accountData)) {
+    // If there is more than one tax account with the specified tax type, use the one whose
+    // name matches the `accountName` param.
+    accountData = accountData.find(account => account.value.toLowerCase() === accountName);
+    if (typeof accountData === 'undefined') {
+      accountNameNotFound = true;
+    }
+  } else if (!accountData || accountData.value.toLowerCase() !== accountName) {
+    accountNameNotFound = true;
+  }
+  if (accountNameNotFound) {
+    throw new TaxAccountNameNotFound(`Cannot find account with name "${accountName}"`, null, {
+      accountName,
+    });
+  }
+  const accountCode = accountData!.name;
+  return accountCode;
+}
+
+/**
+ * Creates a task to get the code of an account whose name and tax type is known. The reason we
+ * would want the code of an account is to generate reports for that account.
  * @returns The account code.
  */
 export default async function getAccountCodeTask({
@@ -54,37 +98,7 @@ export default async function getAccountCodeTask({
     task,
     async func() {
       task.status = 'Getting tax account information';
-
-      // Get the codes and names of all tax accounts that have the specified tax type.
-      const response = await xmlRequest({
-        url: `${ZraDomain}/frontController.do?actionCode=RPRTPAJAXCHILDCOMBO`,
-        method: 'post',
-        data: {
-          prm_ajaxComboTarget: 'accountName',
-          reportCode: ReportCode.PENDING_LIABILITY,
-          prm_TaxType: taxTypeId,
-        },
-      });
-
-      let accountData: AccountData | AccountData[] | undefined = response['request-params'].param;
-      let accountNameNotFound: boolean = false;
-      if (Array.isArray(accountData)) {
-        // If there is more than one tax account with the specified tax type, use the one whose
-        // name matches the `accountName` param.
-        accountData = accountData.find(account => account.value.toLowerCase() === accountName);
-        if (typeof accountData === 'undefined') {
-          accountNameNotFound = true;
-        }
-      } else if (!accountData || accountData.value.toLowerCase() !== accountName) {
-        accountNameNotFound = true;
-      }
-      if (accountNameNotFound) {
-        throw new TaxAccountNameNotFound(`Cannot find account with name "${accountName}"`, null, {
-          accountName,
-        });
-      }
-      const accountCode = accountData!.name;
-      return accountCode;
+      return getTaxAccountCode({ accountName, taxTypeId });
     },
   });
 }
